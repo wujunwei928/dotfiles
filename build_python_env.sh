6b82e5dbff1b3e4ee37c06867854df48011aa8ef
wujunwei <1399952803@qq.com>
2016-09-29 00:37:59 +0800

    python环境搭建之安装需要的python包

--- build_python_env.sh
+++ build_python_env.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
-pip install PyMySQL
+# 安装数据挖掘相关包
+pip install pandas scikit-learn scikit-image
+
+# 安紫自然语言处理
+pip install jieba nltk wordcloud
+
+# 安装爬虫相关
+pip install scrapy pyspider
+
+# 安装sql-nosql相关：
+pip install PyMySQL MySQLdb redis pymongo
+
+# 安装虚拟环境相关
+pip install virtualenv
+
+# 安装运维相关
+pip install psutil pexpect fabric ansible
+
+# 安装web开发相关
+pip install diango flask
+
+# 安装特色包
+pip install httpie thefuck sh
