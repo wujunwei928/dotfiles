7b3b8cb0fdb11ef24d14b4762d222def4c55234c
wujunwei928 <1399952803@qq.com>
2016-09-29 10:55:00 +0800

    添加自动化运维相关处理模块,
    系统性能信息模块 psutil, 
    IP地址处理模块 ipy
    DNS处理模块 dnspython

--- build_python_env.sh
+++ build_python_env.sh
@@ -15,8 +15,19 @@ pip install PyMySQL redis pymongo
 # 安装虚拟环境相关
 pip install virtualenv
 
-# 安装运维相关
-pip install psutil pexpect fabric ansible
+# 安装自动化运维相关
+pip install pexpect fabric ansible  # 自动部署相关
+pip install psutil  # 系统性能信息模块
+pip install ipy  # IP地址处理模块
+pip install dnspython  # DNS处理模块
+
+# python操作excel
+pip install XlsxWriter
+
+# python开发MapReduce
+pip install mrjob
+
+
 
 # 安装web开发相关
 pip install diango flask
